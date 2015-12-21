70cbc66f350f90a1fadf827cb2f44ab21fb8604d
Anand Dhavle <ananddhavle@gmail.com>
2015-12-21 12:48:48 +0530

    Add time pause ability

--- Assets/Source code/Callback.cs
+++ Assets/Source code/Callback.cs
@@ -1,4 +1,4 @@
-﻿public delegate void Callback();
+public delegate void Callback();
 public delegate void Callback<T>(T arg1);
 public delegate void Callback<T, U>(T arg1, U arg2);
 public delegate void Callback<T, U, V>(T arg1, U arg2, V arg3);
@@ -8,4 +8,11 @@ public enum EVENTID
     EVENT_REWIND_TIME,
     //Use this to count number of events
     COUNT,
+}
+
+public enum MANIPULATION_TYPE
+{
+    NONE,
+    REWIND,
+    PAUSE,
 }
--- Assets/Source code/RewindBehaviour.cs
+++ Assets/Source code/RewindBehaviour.cs
@@ -7,24 +7,29 @@ public class RewindBehaviour : MonoBehaviour
     public float totalRewindTime = 5f;
 
     ObjectData[] storedData;
+    Vector3 prevVelocity;
     int i = 0;
     int rewindIndex;
     int maxIndex;
     bool b_rewind;
+    Rigidbody2D m_rigidbody;
+    RigidbodyConstraints2D initalConstraint;
+    MANIPULATION_TYPE currentManipulationType;
 
     void Start()
     {
         storedData = new ObjectData[(int)(totalRewindTime/Time.fixedDeltaTime)];
         maxIndex = storedData.Length;
-        Messenger.AddListener(EVENTID.EVENT_REWIND_TIME, OnEventRewindTime);
+        Messenger.AddListener<MANIPULATION_TYPE>(EVENTID.EVENT_REWIND_TIME, OnEventRewindTime);
         b_rewind = false;
+        m_rigidbody = GetComponent<Rigidbody2D>();
     }
 
     void FixedUpdate()
     {
         if (b_rewind)
             RewindTime();
-        else
+        else if(currentManipulationType != MANIPULATION_TYPE.PAUSE)
             StoreObjectData();
     }
 
@@ -50,15 +55,47 @@ public class RewindBehaviour : MonoBehaviour
             storedData[rewindIndex] = null;
         }
         else
-            Messenger.Broadcast(EVENTID.EVENT_REWIND_TIME);
+        {
+            m_rigidbody.velocity = Vector2.zero;
+            Messenger.Broadcast<MANIPULATION_TYPE>(EVENTID.EVENT_REWIND_TIME, MANIPULATION_TYPE.NONE);
+        }
     }
 
-    void OnEventRewindTime()
+    void OnEventRewindTime(MANIPULATION_TYPE type)
     {
-        rewindIndex = i;
-        b_rewind = !b_rewind;
+        switch(type)
+        {
+            case MANIPULATION_TYPE.REWIND:
+                currentManipulationType = currentManipulationType == MANIPULATION_TYPE.REWIND ? MANIPULATION_TYPE.NONE : MANIPULATION_TYPE.REWIND;
+                rewindIndex = i;
+                b_rewind = !b_rewind;
+                break;
+
+            case MANIPULATION_TYPE.NONE:
+                b_rewind = !b_rewind;
+                break;
+
+            case MANIPULATION_TYPE.PAUSE:
+                pauseTime();
+                break;
+        }
     }
 
+    void pauseTime()
+    {
+        currentManipulationType = currentManipulationType == MANIPULATION_TYPE.PAUSE ? MANIPULATION_TYPE.NONE : MANIPULATION_TYPE.PAUSE;
+        if (m_rigidbody.constraints == RigidbodyConstraints2D.FreezeAll)
+        {
+            m_rigidbody.velocity = prevVelocity;
+            m_rigidbody.constraints = initalConstraint;
+        }
+        else
+        {
+            prevVelocity = m_rigidbody.velocity;
+            initalConstraint = m_rigidbody.constraints;
+            m_rigidbody.constraints = RigidbodyConstraints2D.FreezeAll;
+        }
+    }
 }
 
 public class ObjectData
